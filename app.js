8090e2b865063f5acea4480948dda84dae8803e0
Qasim509 <hq74248@gmail.com>
2025-11-23 03:42:08 -0800

    i

--- app.js
+++ app.js
@@ -1,102 +1,33 @@
-var userName = document.getElementById('name')
-var email = document.getElementById('email')
-var password = document.getElementById('password')
-var age = document.getElementById('age')
-var contact = document.getElementById('contact')
-var cnic = document.getElementById('cnic')
+gallery = [
+    "https://sm.ign.com/ign_pk/review/g/god-of-war/god-of-war-ragnarok-review_v5s5.jpg",
+    "https://static0.polygonimages.com/wordpress/wp-content/uploads/chorus/uploads/chorus_asset/file/10137177/49c7d29293f89dfef95969fde62f5c27e7cc780c.jpg?w=1600&h=900&fit=crop",
+    "https://cdn1.epicgames.com/spt-assets/330dace5ffc74156987f91d454ac544b/project-w-1kt2x.jpg",
+    "https://cdn.mos.cms.futurecdn.net/UcXeK6DWKBWdc3Ao4TZ9nU.jpg"
+]
 
-var userNameError = document.getElementById('nameError')
-var emailError = document.getElementById('emailError')
-var passwordError = document.getElementById('passwordError')
-var ageError = document.getElementById('ageError')
-var contactError = document.getElementById('contactError')
+var currentIndex = 0
+var currentImg = document.getElementById('imgSlider')
+var prevButton = document.getElementById('previousBtn')
+var nextButton = document.getElementById('nextBtn')
 
-function submit(){
-    var hasError = false
-
-    //Name validation
-
-    if (userName.value == ""){
-        userNameError.innerText = 'Please enter your name.'
-        hasError = true
+function nextImg(){
+    currentIndex += 1
+    currentImg.src = gallery[currentIndex]
+    if(currentIndex == 1){
+        prevButton.style.display = "inline"
     }
-    else if(userName.value.length < 5){
-        userNameError.innerText = "Username must be greater than 5 letters."
-        hasError = true
-    }
-    else{
-        userNameError.innerText = ""
-        hasError = false
+    if (currentIndex == 3){
+        nextButton.style.display = "none"
     }
 
-    //Email validation
-
-    if (email.value == ""){
-        emailError.innerText = "Please enter your email address."
-        hasError = true
+}
+function prevImg(){
+    currentIndex -= 1
+    currentImg.src = gallery[currentIndex]
+    if(currentIndex == 2){
+        nextButton.style.display = "inline"
     }
-    else if(!email.value.includes("@")){
-        emailError.innerText = "Enter correct email address."
-        hasError = true
+    if(currentIndex == 0){
+        prevButton.style.display = "none"
     }
-    else{
-        emailError.innerText = ""
-        hasError = false
-    }
-
-    //Password validation
-
-    if(password.value == ""){
-        passwordError.innerText = "Please enter a password"
-        hasError = true
-    }
-    else if(password.value.length < 8){
-        passwordError.innerText = "Password must be greater than or equal to 8 characters"
-        hasError = true
-    }
-    else{
-        passwordError.innerText = ""
-        hasError = false
-    }
-
-    //Age error
-    if(age.value == 0){
-        ageError.innerText = "Please enter your age"
-        hasError = true
-    }
-    else if(age.value < 18){
-        ageError.innerText = "Your age must be greater than 18 to be eligible for cnic"
-        hasError = true
-    }
-    else{
-        ageError.innerText = ""
-        hasError = false
-    }
-
-    //contact validation
-
-    if(contact.value == 0){
-        contactError.innerText = "Please enter your contact number"
-        hasError = true
-    }
-    else if(contact.value.toString().length != 11){
-        contactError.innerText = "Contact number must be of 11 numbers"
-        hasError = true
-    }
-    else{
-        contactError.innerText = ""
-        hasError = false
-    }
-
-    //CNIC Generation
-
-    if (hasError == false){
-        var cnicNo = (Math.random()*10000000000000).toFixed()
-        cnicNo = cnicNo.toString().substring(0,4)+"-"+ cnicNo.toString().substring(4,12)+"-"+cnicNo.toString().substring(12,13)
-        // console.log(cnic)
-        cnic.value = cnicNo
-
-    }
-    
-
 }
